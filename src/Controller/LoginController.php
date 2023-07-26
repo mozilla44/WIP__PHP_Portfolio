92dbcfaaaf78cbb88f65fbfabcc3be3254d9d94f
Antoine <antoine.frisco@gmail.com>
2023-07-26 16:19:58 +0200

    prevent logout if not connected

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -6,15 +6,17 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
 
 class LoginController extends AbstractController
-{
+{   
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
+        if ($this->getUser()) {
+             return $this->redirectToRoute('home');
+        }
 
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
@@ -24,8 +26,11 @@ class LoginController extends AbstractController
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
 
+    #[IsGranted('ROLE_USER')]
     #[Route(path: '/logout', name: 'logout')]
+
     public function logout(): void
+    
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
